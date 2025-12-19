90c5e2495fd855faca0f49e256f180a7212a6027
shristi482004 <shristish484@gmail.com>
2025-12-19 20:13:21 +0530

    explore express basics: middleware vs error middleware, route params, centralized error handling, and backend-only workflow

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const http=require('http');
-const server=http.createServer((req,res,err)=>{
-    res.end("Hello from the other side.How ypu doin?");
-    if(err) console.error(err.message);
-
+const express=require('express');
+const app=express();
+/*app.use((err,req,res,next)=>{
+    if(err) res.status(500).send("Uh Oh. Something broke!");
+    else console.log("No errors found");
+   
+})
+*/
+app.use((req,res,next)=>{
+console.log("This is a middleware");
+next();
+})
+app.use((req,res,next)=>{
+console.log("This is a middleware 2");
+next();
 });
-server.listen(3000,()=>{
+
+app.get("/",(req,res)=>{
+    res.send("What are you upto?");
+})
+app.get("/nice",(req,res)=>{
+    res.send("What are you upto?");
+})
+app.get("/profile/:name",(req,res)=>{
+    res.send("Hello there Ms."+req.params.name);
+})
+app.get("/profile/:name/:age",(req,res)=>{
+    res.send("Hello there Ms."+req.params.name+", of age "+req.params.age);
+})
+app.get("/information",(req,res,next)=>{
+    return next(new Error("This is an error"));
+})
+//error handler must be at the last
+app.use((err,req,res,next)=>{
+    if(err) {console.log(err.message);
+        res.status(500).send("Uh Oh. Something broke!");}
+    else console.log("No errors found");
+   
+})
+app.listen(3000,()=>{
     console.log("Server listening on port 3000");
 });
